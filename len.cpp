c9272664fa1fee1d286a83e199149edfcaa83689
Khushi Yadav <kky957242@gmail.com>
2024-12-24 18:43:27 +0530

    Create len.cpp

---
+++ len.cpp
@@ -0,0 +1,14 @@
+String Length
+To get the length of a string, use the length() function
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  cout << "The length of the txt string is: " << txt.length();
+  return 0;
+}
+
+then we get the output is -- "The length of the txt string is: 26
