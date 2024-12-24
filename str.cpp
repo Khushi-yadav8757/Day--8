3231a85f669ee6362a4049dbce2e158c3d3fbede
Khushi Yadav <kky957242@gmail.com>
2024-12-24 18:45:58 +0530

    Create str.cpp

---
+++ str.cpp
@@ -0,0 +1,13 @@
+You might see some C++ programs that use the size() function to get the length of a string. 
+This is just an alias of length(). 
+It is completely up to you if you want to use length() or size() .
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  cout << "The length of the txt string is: " << txt.size();
+  return 0;
+}
