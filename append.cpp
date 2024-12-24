ac203ffb3ea5c26b222bbd282f69a7a137dcacc6
Khushi Yadav <kky957242@gmail.com>
2024-12-24 18:34:53 +0530

    Create append.cpp

---
+++ append.cpp
@@ -0,0 +1,15 @@
+Append
+A string in C++ is actually an object, which contain functions that can perform certain operations on strings.
+For example, you can also concatenate strings with the append() function:
+
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+int main () {
+  string firstName = "John ";
+  string lastName = "Doe";
+  string fullName = firstName.append(lastName);
+  cout << fullName;
+  return 0;
+}
